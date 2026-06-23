244e3955a66bccbf2ed09c413824bd9aa44fd5c7
Deivid Oliveira <deividvs@MacBook-Air-de-Deivid.local>
2026-06-23 10:04:39 -0300

    Update VTurb video embed

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,7 +12,7 @@ export const viewport = {
 }
 
 const PLAYER_ACCOUNT = 'fae52404-8fd3-4f9c-8167-946eb654f0d6'
-const PLAYER_ID = '681696116fd9eb4b1a3122ec'
+const PLAYER_ID = '6a3a81f61fa755ae788ed0f8'
 
 export default function RootLayout({ children }) {
   return (
@@ -36,26 +36,11 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           rel="stylesheet"
         />
 
-        {/* VTurb / converteai smartplayer preloads */}
-        <link
-          rel="preload"
-          href={`https://scripts.converteai.net/${PLAYER_ACCOUNT}/players/${PLAYER_ID}/v4/embed.html`}
-        />
+        {/* VTurb / converteai smartplayer */}
         <link
           rel="preload"
           as="script"
           href={`https://scripts.converteai.net/${PLAYER_ACCOUNT}/players/${PLAYER_ID}/v4/player.js`}
-        />
-        <link
-          rel="preload"
-          as="script"
-          href="https://scripts.converteai.net/lib/js/smartplayer-wc/v4/smartplayer.js"
-        />
-        <link
-          rel="preload"
-          as="fetch"
-          crossOrigin=""
-          href={`https://cdn.converteai.net/${PLAYER_ACCOUNT}/681695f65a7f296ab51f0b29/main.m3u8`}
         />
         <link rel="dns-prefetch" href="https://cdn.converteai.net" />
         <link rel="dns-prefetch" href="https://scripts.converteai.net" />
@@ -73,15 +58,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           />
         </noscript>
         {/* End Google Tag Manager (noscript) */}
-
-        {/* smartplayer perf marker + SDK (run before hydration) */}
-        <Script id="smartplayer-plt" strategy="beforeInteractive">
-          {`!function(i,n){i._plt=i._plt||(n&&n.timeOrigin?n.timeOrigin+n.now():Date.now())}(window,performance);`}
-        </Script>
-        <Script
-          src="https://scripts.converteai.net/lib/js/smartplayer-wc/v4/sdk.js"
-          strategy="beforeInteractive"
-        />
         {children}
       </body>
     </html>
--- components/VideoPlayer.jsx
+++ components/VideoPlayer.jsx
@@ -2,61 +2,30 @@
 
 import React from 'react'
 
-/* VTurb / converteai player embed (lazy iframe, 16:9).
- *
- * Isolated in its own component and mounted imperatively so React never
- * reconciles the iframe while the chat re-renders for new messages/typing. */
-
-const PLAYER_ID = '681696116fd9eb4b1a3122ec'
+const PLAYER_ID = '6a3a81f61fa755ae788ed0f8'
 const PLAYER_ACCOUNT = 'fae52404-8fd3-4f9c-8167-946eb654f0d6'
+const SMARTPLAYER_ID = `vid-${PLAYER_ID}`
+const SCRIPT_ID = `vturb-player-${PLAYER_ID}`
 
 export default class VideoPlayer extends React.Component {
-  constructor(props) {
-    super(props)
-    this.mountRef = React.createRef()
-    this.iframe = null
-  }
-
-  shouldComponentUpdate() {
-    return false
-  }
-
   componentDidMount() {
-    if (!this.mountRef.current || this.iframe) return
+    if (document.getElementById(SCRIPT_ID)) return
 
-    const wrapper = document.createElement('div')
-    wrapper.id = `ifr_${PLAYER_ID}_wrapper`
-    wrapper.style.cssText = 'margin:0 auto;width:100%;'
-
-    const aspect = document.createElement('div')
-    aspect.id = `ifr_${PLAYER_ID}_aspect`
-    aspect.style.cssText = 'position:relative;padding:56.074766355140184% 0 0 0;'
-
-    const iframe = document.createElement('iframe')
-    const embedUrl = new URL(`https://scripts.converteai.net/${PLAYER_ACCOUNT}/players/${PLAYER_ID}/v4/embed.html`)
-    const params = new URLSearchParams(window.location.search)
-    params.delete('xcod')
-    params.set('vl', window.location.href)
-    embedUrl.search = params.toString()
-
-    iframe.id = `ifr_${PLAYER_ID}`
-    iframe.src = embedUrl.toString()
-    iframe.frameBorder = '0'
-    iframe.allowFullscreen = true
-    iframe.referrerPolicy = 'origin'
-    iframe.style.cssText = 'position:absolute;top:0;left:0;width:100%;height:100%;border:0;'
-
-    aspect.appendChild(iframe)
-    wrapper.appendChild(aspect)
-    this.mountRef.current.appendChild(wrapper)
-    this.iframe = iframe
-  }
-
-  componentWillUnmount() {
-    this.iframe = null
+    const script = document.createElement('script')
+    script.id = SCRIPT_ID
+    script.src = `https://scripts.converteai.net/${PLAYER_ACCOUNT}/players/${PLAYER_ID}/v4/player.js`
+    script.async = true
+    document.head.appendChild(script)
   }
 
   render() {
-    return <div ref={this.mountRef} style={{ width: '100%' }} />
+    return React.createElement('vturb-smartplayer', {
+      id: SMARTPLAYER_ID,
+      style: {
+        display: 'block',
+        margin: '0 auto',
+        width: '100%',
+      },
+    })
   }
 }
